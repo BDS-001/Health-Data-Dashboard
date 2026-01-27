739ca6c0159a3afd9a004d06d7456fd4549e66b8
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-26 22:47:13 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+testData = {
+    'id': [0,1,2,3,4],
+    'names':['bob', 'foo', 'bar', 'baz', 'jack'],
+    'blood_pressure': [120, 115, 145, 110, 135],
+}
+
+df = pd.DataFrame(testData)
+
+st.write('Patient Data')
+st.dataframe(df)
+
+high_bp = df[df['blood_pressure'] > 130]
+st.write("Patients with BP > 130:")
+st.dataframe(high_bp)
+
+fig = px.bar(high_bp, x='names', y='blood_pressure', title='test graph')
+st.plotly_chart(fig)
