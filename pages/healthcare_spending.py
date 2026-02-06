33cad11962694d4aec1117684af78dd1fb435d34
BDS <113209967+BDS-001@users.noreply.github.com>
2026-02-05 20:53:09 -0500

    update: setup remaining columns to follow the first one pattern

--- pages/healthcare_spending.py
+++ pages/healthcare_spending.py
@@ -28,9 +28,26 @@ with col1:
 with col2:
     st.header('Spending GDP')
     gdp_data = spending_data.get_spending_gdp()
+    life_exp_data = life_data.get_both()
+    combined = pd.merge(gdp_data, life_exp_data, on='Year')
+    fig = px.scatter(combined, x='Life Expectancy', y='Spending', text='Year')
+    fig.update_traces(textposition='top center')
+    st.plotly_chart(fig, width='stretch')
+
 with col3:
     st.header('Out of Pocket')
     out_of_pocket_data = spending_data.get_out_of_pocket()
+    life_exp_data = life_data.get_both()
+    combined = pd.merge(out_of_pocket_data, life_exp_data, on='Year')
+    fig = px.scatter(combined, x='Life Expectancy', y='Spending', text='Year')
+    fig.update_traces(textposition='top center')
+    st.plotly_chart(fig, width='stretch')
+
 with col4:
     st.header('Gov Spending')
     gov_data = spending_data.get_gov_spending()
+    life_exp_data = life_data.get_both()
+    combined = pd.merge(gov_data, life_exp_data, on='Year')
+    fig = px.scatter(combined, x='Life Expectancy', y='Spending', text='Year')
+    fig.update_traces(textposition='top center')
+    st.plotly_chart(fig, width='stretch')
